6ab6bc73e5d2a4b2a2cd949a870f69e110973058
Alfred Boateng <programmingwithalfred@gmail.com>
2022-10-04 09:47:29 +0000

    Update Person class

--- person.rb
+++ person.rb
@@ -1,27 +1,44 @@
 require 'securerandom'
+require './nameable.rb'
+require './capitalize.rb'
+require './trimmer.rb'
+require './decorator.rb'
 
-class Person
-  attr_accessor :name, :age
-  attr_reader :id
+class Person < Nameable
+  attr_accessor :name, :age, :id
+  attr_reader :parent_permission
 
-  def initialize(age, name = 'Unkown', parent_permission: true)
-    @id = SecureRandom.uuid
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @name = name
-    @parent_permission = parent_permission
     @age = age
+    @parent_permission = parent_permission
+    @id = SecureRandom.uuid
+    super()
   end
 
   def of_age?
-    return true if @age >= 18
-
-    false
+    @age >= 18
   end
 
-  def can_use_services
-    return true if of_age? || @parent_permission
+  def can_use_services?
+    of_age? || @parent_permission
+  end
 
-    false
+  def capitalize
+    @name.capitalize
+  end
+
+  def correct_name
+    @name
   end
 
   private :of_age?
 end
+
+person = Person.new(22, 'maximilianus')
+capitalized_person = CapitalizeDecorator.new(person)
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+
+puts person.correct_name
+puts capitalized_person.correct_name
+puts capitalized_trimmed_person.correct_name
